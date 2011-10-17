7992d15f1398a4f95a4b0caa7f1accc88c27f5d4
Joachim Bengtsson <joachimb@gmail.com>
2011-10-17 23:03:48 +0200

    Sound resources. Requires git submodule update --init!
    * Added a DTSound resource
    * Uses third party OpenAL wrapper Finch (my fork)
    * Cleaned up project a bit; sorry for doing that in this commit
    * Added a few test sounds

---
+++ Deathtroid/Sound/DTSound.h
@@ -0,0 +1,14 @@
+//
+//  DTSound.h
+//  Deathtroid
+//
+//  Created by Joachim Bengtsson on 2011-10-17.
+//  Copyright (c) 2011 Third Cog Software. All rights reserved.
+//
+
+#import "DTResource.h"
+#import "FISound.h"
+
+@interface DTSound : DTResource
+-(FISound*)newVoice;
+@end
---
+++ Deathtroid/Sound/DTSound.m
@@ -0,0 +1,61 @@
+//
+//  DTSound.m
+//  Deathtroid
+//
+//  Created by Joachim Bengtsson on 2011-10-17.
+//  Copyright (c) 2011 Third Cog Software. All rights reserved.
+//
+
+#import "DTSound.h"
+#import "FIFactory.h"
+#import "../Decoder/FISoundDecoder.h"
+
+@interface DTSound ()
+@property(nonatomic,strong) FISoundSample *sample;
+@end
+
+@implementation DTSound
+@synthesize sample;
+-(FISound*)newVoice;
+{
+    return [[FISound alloc] initWithSample:sample error:NULL];
+}
+@end
+
+@interface DTSoundLoader : DTResourceLoader
+@property(nonatomic, strong) FIFactory *factory;
+@end
+
+@implementation DTSoundLoader
+@synthesize factory;
++(void)load{
+	[DTResourceManager registerResourceLoader:self withTypeName:@"sound"];
+}
+-(id)init;
+{
+    if(!(self = [super init])) return nil;
+    
+    self.factory = [FIFactory new];
+    return self;
+}
+-(id<DTResource>)loadResourceAtURL:(NSURL *)url usingManager:(DTResourceManager *)manager;
+{
+	[super loadResourceAtURL:url usingManager:manager];
+	NSString *soundPath = [self.definition objectForKey:@"file"];
+	NSURL *soundUrl = [NSURL URLWithString:soundPath relativeToURL:url];
+	
+	DTSound *sound = [[DTSound alloc] initWithResourceId:url.dt_resourceId];
+    
+    NSError *err = nil;
+    id<FISoundDecoder> dec = [factory decoderForFileAtPath:soundUrl.path];
+    sound.sample = [dec decodeFileAtPath:soundUrl.path error:&err];
+    if(!sound.sample) {
+        // TODO<nevyn>: Pass NSError to caller instead; this is not a programmer error.
+        [NSException raise:NSInvalidArgumentException format:@"Decoding error: %@", err];
+        return nil;
+    }
+    
+    return sound;
+    
+}
+@end
